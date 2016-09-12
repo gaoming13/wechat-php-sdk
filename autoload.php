268b73f9258ef27b6f51d24885d9d44703809d0b
zhaoliming <gaoming13@yeah.net>
2016-09-12 11:00:10 +0800

    添加autoload.php自动加载方法

---
+++ autoload.php
@@ -0,0 +1,6 @@
+<?php
+spl_autoload_register(function ($class) {
+    if (false !== stripos($class, 'Gaoming13\WechatPhpSdk')) {      
+        require_once __DIR__.'/src/'.str_replace('\\', DIRECTORY_SEPARATOR, substr($class, 10)).'.class.php';
+    }
+});
